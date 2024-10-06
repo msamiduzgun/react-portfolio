55c57d4bbbc6c1dc18849951b5ef5685ad2f5890
Sami <flysamihd@gmail.com>
2024-10-06 22:09:27 +0300

    küçük bir güncelleme

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -28,7 +28,7 @@ const Technologies = () => {
         transition={{ duration: 1.5 }}
         className="my-20 text-center text-4xl"
       >
-        Technologies
+        Teknolojiler
       </motion.h2>
       <motion.div
         whileInView={{ opacity: 1, x: 0 }}
